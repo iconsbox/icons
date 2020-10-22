7933d8371f0b21a630beda85b10b8bec0da74bb5
jafarRezaei <bomber.man87@yahoo.com>
2020-10-22 22:30:01 +0330

    v2.0.1

--- packages/Medical/src/index.js
+++ packages/Medical/src/index.js
@@ -1,8 +1,8 @@
 /**
 * THIS IS AN AUTO GENERATED FILE, CHANGES COULD BE OVERWRITE
 */
-export { default as AmbulanceIcon } from './AmbulanceIcon/component';
 export { default as AnatomyIcon } from './AnatomyIcon/component';
+export { default as AmbulanceIcon } from './AmbulanceIcon/component';
 export { default as BagIcon } from './BagIcon/component';
 export { default as BandageIcon } from './BandageIcon/component';
 export { default as BottleIcon } from './BottleIcon/component';
@@ -22,7 +22,7 @@ export { default as InjectionIcon } from './InjectionIcon/component';
 export { default as MedicineIcon } from './MedicineIcon/component';
 export { default as NavigationIcon } from './NavigationIcon/component';
 export { default as ReportIcon } from './ReportIcon/component';
-export { default as ResearchIcon } from './ResearchIcon/component';
 export { default as SpermIcon } from './SpermIcon/component';
+export { default as ResearchIcon } from './ResearchIcon/component';
 export { default as SpiritIcon } from './SpiritIcon/component';
 export { default as TemperatureIcon } from './TemperatureIcon/component';
