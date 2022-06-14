8db39bf981eef1921257219c6d2f4e4ddf2f3bcb
sayjeyhi <sayjeyhi@gmail.com>
2022-06-14 19:04:55 +0430

    fix versions

--- scripts/release/change-version.js
+++ scripts/release/change-version.js
@@ -4,8 +4,6 @@ const changeVersionInPackageJson = require('../version/change-version');
 const myArguments = process.argv.slice(2);
 const passedVersion = myArguments[0] || '--canary';
 
-console.log(`- releasing ${passedVersion} !`);
-
 (async function run() {
   try {
     await changeVersionInPackageJson(passedVersion);
