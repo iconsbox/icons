5a1b93d55b565a333b8c8617f88370b1972652dc
sayjeyhi <sayjeyhi@gmail.com>
2022-06-16 16:30:55 +0430

    remove shelljs

--- scripts/release/release.js
+++ scripts/release/release.js
@@ -1,23 +1,23 @@
-/* eslint-disable no-console */
-const shell = require('shelljs');
-const path = require('path');
-const grabVersionAndGenerateNewOne = require('../version/version-generator');
-
-const myArguments = process.argv.slice(2);
-const passedVersion = myArguments[0] || '--patch';
-
-console.log(`releasing ${passedVersion} together !`);
-
-(async function run() {
-  try {
-    const { version } = await grabVersionAndGenerateNewOne(passedVersion, path.join(process.cwd(), 'packages/Ant/'));
-
-    if (shell.exec(`lerna run --parallel change-version -- -- ${version} && lerna run --parallel publish-npm -- -- ${version}`).code !== 0) {
-      shell.echo('Release build, or publish failed');
-      shell.exit(1);
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-}());
+// /* eslint-disable no-console */
+// const shell = require('shelljs');
+// const path = require('path');
+// const grabVersionAndGenerateNewOne = require('../version/version-generator');
+//
+// const myArguments = process.argv.slice(2);
+// const passedVersion = myArguments[0] || '--patch';
+//
+// console.log(`releasing ${passedVersion} together !`);
+//
+// (async function run() {
+//   try {
+//     const { version } = await grabVersionAndGenerateNewOne(passedVersion, path.join(process.cwd(), 'packages/Ant/'));
+//
+//     if (shell.exec(`lerna run --parallel change-version -- -- ${version} && lerna run --parallel publish-npm -- -- ${version}`).code !== 0) {
+//       shell.echo('Release build, or publish failed');
+//       shell.exit(1);
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+// }());
