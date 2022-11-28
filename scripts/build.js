d77f88421400d2edf07356481be1de2ec1dcd08b
sayjeyhi <sayjeyhi@gmail.com>
2022-11-28 10:20:42 +0400

    fix: use separate thread to build CJS

--- scripts/build.js
+++ scripts/build.js
@@ -2,31 +2,28 @@
 const esbuild = require('esbuild');
 const glob = require('glob');
 
-Promise
-  .all([
-    esbuild.build({
-      entryPoints: glob.sync('src/**/*.{tsx,ts}'),
-      outdir: "dist",
-      minify: false,
-      platform: 'browser',
-      sourcemap: false,
-      sourcesContent: false,
-    }),
-    esbuild.build({
-      entryPoints: glob.sync('src/**/*.{tsx,ts}'),
-      outdir: "dist",
-      outExtension: {
-        ".js": ".cjs.js"
-      },
-      minify: false,
-      platform: 'node',
-      format: 'cjs',
-      sourcemap: false,
-      sourcesContent: false,
-    }),
-  ])
-  .then(() => console.log("⚡ Javascript build complete! ⚡"))
-  .catch(error => {
-    console.error(error)
-    process.exit(1)
+!(async function () {
+  await esbuild.build({
+    entryPoints: glob.sync('src/**/*.{tsx,ts}'),
+    outdir: "dist",
+    minify: false,
+    platform: 'browser',
+    sourcemap: false,
+    sourcesContent: false,
   });
+
+  await esbuild.build({
+    entryPoints: glob.sync('src/**/*.{tsx,ts}'),
+    outdir: "dist",
+    outExtension: {
+      ".js": ".cjs.js"
+    },
+    minify: false,
+    platform: 'node',
+    format: 'cjs',
+    sourcemap: false,
+    sourcesContent: false,
+  });
+
+  console.log("⚡ Javascript build complete! ⚡")
+})();
